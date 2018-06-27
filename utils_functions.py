c2473ba76e8d55c55fb7f6101eea84ae91b714fc
Cady Patino <cnspatino@gmail.com>
2018-06-27 16:52:46 -0500

    Added load_data and process_image functions to utils_functions.py

---
+++ utils_functions.py
@@ -0,0 +1,77 @@
+def load_data(data_dir):
+    """
+    This function loads a data directory and performs the necessary transforms to produce a 
+    dataloader for the training and validation sets.
+    
+    Input: data directory that contains a 'train' and 'valid' folder with png images like follows:
+                training set: data_dir/train/sample.png
+                validation set: data_dir/valid/sample.png
+    Returns: trainloader and validloader to be used for model training
+    """
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+
+    # definite transforms for training and validation sets
+    train_transforms = transforms.Compose([transforms.RandomResizedCrop(224),
+                                       transforms.RandomRotation(40),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229,0.224,0.225])
+                                      ])
+
+    valid__transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406],
+                                                           [0.229,0.224,0.225])
+                                     ])
+
+    # load the datasets using ImageFolder
+    train_dataset = datasets.ImageFolder(root = train_dir, transform = train_transforms)
+    valid_dataset = datasets.ImageFolder(root = valid_dir, transform = valid_test_transforms)
+
+    # define the dataloaders using the image datasets and transforms
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size = 50, shuffle=True)
+    validloader = torch.utils.data.DataLoader(valid_dataset, batch_size = 50, shuffle=True)
+    
+    return train_dataset, valid_dataset, trainloader, validloader
+
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model.
+        Returns a Numpy array.
+    '''
+    
+    # load image and get size
+    im = Image.open(image)
+    width, height = im.size
+    
+    
+    # resize image so shortest side is 256 pixels
+    if width < height:
+        im.thumbnail((255, height))
+    else:
+        im.thumbnail((width, 255))
+    
+    # get new width and height to define crop box
+    width, height = im.size
+    crop_box = ((width - 224)/2, (height - 224)/2, (width + 224)/2, (height + 224)/2)
+    
+    # crop image in center
+    im = im.crop(crop_box)
+    
+    # convert image to numpy array and convert values to floats 0 to 1
+    np_image = np.array(im)
+    np_image = np_image/255
+    
+    # normalize the color channels
+    norm_mean = np.array([0.485, 0.456, 0.406])
+    norm_std = np.array([0.229, 0.224, 0.225])
+    np_image = (np_image - norm_mean) / norm_std
+    
+    # transpose image array so that color channel is first dimension
+    processed_image = np_image.transpose((2,0,1))
+
+    return processed_image
+
