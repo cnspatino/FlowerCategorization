04ebfd8385889fa3f4ce12aef94d5e98215f05bb
Cady Patino <cnspatino@gmail.com>
2018-06-28 22:24:57 -0500

    Modified script for saving checkpoint to include model architecture

--- train.py
+++ train.py
@@ -2,27 +2,28 @@
 This script will train a new network on a given dataset of images using transfer learning
 with a pre-trained model. It will then save the newly trained model as a checkpoint.
 
-Input: data directory with image data sets (train and validation sets in separate folders)
-Optional inputs:    - save directory (if different from data directory)
-                    - pre-trained model architecture (default is vgg19)
-                    - learning rate (default is 0.001)
-                    - number of units in hidden layer (default is 2500)
-                    - number of epochs (default is 3)
-                    - whether to train on gpu (defailt is cpu)
+Required input:     - data_dir: data directory with image data sets (train and validation sets in separate folders,
+                      root/train and root/valid)
 
+Optional inputs:    - save_dir: directory in which to save the model checkpoint after training (default is data directory)
+                    - arch: pre-trained model architecture (default is vgg19)
+                    - learning_rate: type float, learning rate for optimizer (default is 0.001)
+                    - hidden_units: type int, number of units in hidden layer (default is 2500)
+                    - epochs: type int, number of epochs for training (default is 3)
+                    - gpu: whether to train on gpu (defailt is cpu)
 """
 
 # set up command line inputs
 import argparse
 
 parser = argparse.ArgumentParser(description='Trains new network on image dataset and saves network to checkpoint')
-parser.add_argument('data_dir')
-parser.add_argument('-s', '--save_dir', help='Save model checkpoints in given directory')
-parser.add_argument('-a','--arch', help='Choose pre-trained VGG model architecture', type=str, default='vgg19')
-parser.add_argument('-lr', '--learning_rate', help='Set learning learning rate', type=int, default=0.001)
-parser.add_argument('-hu', '--hidden_units', help='Set number of hidden units', type=int, default=2500)
-parser.add_argument('-e', '--epochs', help='Set number of epochs', type=int, default=3)
-parser.add_argument('-g', '--gpu', help='Use GPU for training instead of CPU', action='store_true')
+parser.add_argument('data_dir', type=str)
+parser.add_argument('--save_dir', help='Save model checkpoints in given directory', type=str)
+parser.add_argument('--arch', help='Choose pre-trained VGG model architecture', type=str, default='vgg19')
+parser.add_argument('--learning_rate', help='Set learning learning rate', type=float, default=0.001)
+parser.add_argument('--hidden_units', help='Set number of hidden units', type=int, default=2500)
+parser.add_argument('--epochs', help='Set number of epochs', type=int, default=3)
+parser.add_argument('--gpu', help='Use GPU for training instead of CPU', action='store_true')
 args = vars(parser.parse_args())
 
 # import other packages
@@ -30,7 +31,6 @@ import numpy as np
 import torch
 from torch import nn
 from torch import optim
-import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 
 from workspace_utils import active_session
@@ -40,12 +40,12 @@ from model_functions import validation, new_classifier, save_checkpoint
 from collections import OrderedDict
 
 # set input args as variables
-data_dir = args.data_dir
-arch = args.arch
-lr = args.learning_rate
-hidden_units = args.hidden_units
-epochs = epochs
-device = torch.device("cuda:0" if args.gpu is True else "cpu")
+data_dir = args['data_dir']
+arch = args['arch']
+lr = args['learning_rate']
+hidden_units = args['hidden_units']
+epochs = args['epochs']
+device = torch.device("cuda:0" if args['gpu'] is True else "cpu")
 
 # load datasets and get trainloader and validloader
 train_dataset, valid_dataset, trainloader, validloader = load_data(data_dir)
@@ -66,10 +66,9 @@ for param in model.parameters():
 
 # define new classifier to replace current model classifier
 input_size = 25088
-hidden_size = hidden_units
 output_size = 102
 
-model.classifier = new_classifier(input_size, hidden_size, output_size)
+model.classifier = new_classifier(input_size, hidden_units, output_size)
 
 # set criterion and optimizer
 criterion = nn.NLLLoss()
@@ -126,21 +125,13 @@ with active_session():
                 model.train()
 
 
-# save checkpoint of current model
+# set class_to_idx mapping for model
 model.class_to_idx = train_dataset.class_to_idx
 
-checkpoint_state = {'input_size': input_size,
-                    'output_size': output_size,
-                    'hidden_layer': hidden_size,
-                    'state_dict': model.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'epochs': epochs,
-                    'class_to_idx': model.class_to_idx
-                   }
-
-if save_dir is not None:
-    save_filepath = save_dir + '/checkpoint.pth'
+# save model checkpoint
+if args['save_dir'] is None:
+    save_filepath = data_dir + '/checkpoint.pth'
 else:
-    save_filepath = data_dir + 'checkpoint.pth'
+    save_filepath = args['save_dir'] + '/checkpoint.pth'
 
-save_checkpoint(checkpoint_state, save_filepath)
+save_checkpoint(model, arch, optimizer, epochs, input_size, output_size, hidden_units, save_filepath)
